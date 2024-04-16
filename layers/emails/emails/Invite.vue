f90e809264e6ffea37b0095b75b65a118afe78b1
Dang Van Thanh <dangvanthanh@dangthanh.org>
2024-04-16 21:48:20 +0700

    feat: update invite and welcome email template

--- layers/emails/emails/Invite.vue
+++ layers/emails/emails/Invite.vue
@@ -1,33 +1,47 @@
 <script setup lang="ts">
 interface Props {
-	name?: string
+  name?: string,
+  company?: string
+  url?: string
 }
 
 const props = withDefaults(defineProps<Props>(), {
-	name: 'Your Name',
+  name: 'Your Name',
+  company: 'Your Company',
+  url: 'https://github.com/dangvanthanh/nuxt-turborepo-boilerplate'
 })
 
 const tailwindConfig = {
-	theme: {
-		extend: {
-			colors: {
-				primary: '#0065da',
-			},
-		},
-	},
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0065da',
+      },
+    },
+  },
 }
 </script>
 
 <template>
   <ETailwind :config="tailwindConfig">
     <EContainer>
-      <EText>Hi {{ props.name }}</EText>
+      <EText>Dear {{ props.name }}</EText>
       <EText>
-        Invite to Nuxt Emails.
+        Wecome to {{ props.company }}! Click the button below to activate your account:
       </EText>
-      <EButton class="bg-primary px-4 py-2 font-medium leading-4 text-white text-center rounded">
-        Get Started
+      <EButton class="bg-primary px-4 py-2 font-medium leading-4 text-white text-center rounded-full">
+        Activate Account
       </EButton>
+      <EText>
+        Button not working? Use this link instead <ELink :href="props.url">Activate account</ELink>
+      </EText>
+      <EText>
+        Thank you.
+      </EText>
+      <EText>
+        The Team.
+      </EText>
+      <EHr />
     </EContainer>
   </ETailwind>
 </template>
--- layers/emails/emails/Welcome.vue
+++ layers/emails/emails/Welcome.vue
@@ -1,33 +1,45 @@
 <script setup lang="ts">
 interface Props {
-	name?: string
+  name?: string
+  company?: string
 }
 
 const props = withDefaults(defineProps<Props>(), {
-	name: 'Your Name',
+  name: 'Your Name',
+  company: 'Your Company',
 })
 
 const tailwindConfig = {
-	theme: {
-		extend: {
-			colors: {
-				primary: '#0065da',
-			},
-		},
-	},
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0065da',
+      },
+    },
+  },
 }
 </script>
 
 <template>
   <ETailwind :config="tailwindConfig">
     <EContainer>
-      <EText>Hi {{ props.name }}</EText>
+      <EText>Dear {{ props.name }}</EText>
       <EText>
-        Welcome to Nuxt Emails.
+        Welcome to {{ props.company }}
       </EText>
-      <EButton class="bg-primary px-4 py-2 font-medium leading-4 text-white text-center rounded">
+      <EText>
+        Thank you for starting your of {{ props.company }} membership.
+      </EText>
+      <EButton class="bg-primary px-4 py-2 font-medium leading-4 text-white text-center rounded-full">
         Get Started
       </EButton>
+      <EText>
+        Best,
+      </EText>
+      <EText>
+        The Team
+      </EText>
+      <EHr />
     </EContainer>
   </ETailwind>
 </template>
