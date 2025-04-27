97938bb382d341d6a319d0a605e4134de3f2a3e9
Dang Van Thanh <dangvanthanh@dangthanh.org>
2025-04-26 21:00:37 -0700

    deps: update trpc, zod, nuxt. resend, typescript. and radix-ui

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,19 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	devtools: { enabled: true },
 	future: {
 		// New directory structure
 		// https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
 		compatibilityVersion: 4,
 	},
 	compatibilityDate: '2024-07-21',
-	devtools: { enabled: true },
 	build: { transpile: ['trpc-nuxt'] },
-	extends: ['@repo/emails', '@repo/ui'],
 	modules: ['nuxt-auth-utils', '@nuxtjs/color-mode'],
 	colorMode: {
 		preference: 'light',
 		classSuffix: '',
 	},
+	extends: ['@repo/emails', '@repo/ui'],
 	runtimeConfig: {
 		public: {
 			githubToken: process.env.PUBLIC_GITHUB_TOKEN,
--- layers/emails/nuxt.config.ts
+++ layers/emails/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: { enabled: true },
 	compatibilityDate: '2024-07-21',
-	devtools: { enabled: true },
 	extends: ['@repo/ui'],
 	modules: ['@vue-email/nuxt'],
 	vueEmail: {
