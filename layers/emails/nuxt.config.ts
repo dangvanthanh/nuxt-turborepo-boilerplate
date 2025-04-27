92c67dd890db35c9d876d0a144aca6bdf58b620f
Dang Van Thanh <dangvanthanh@dangthanh.org>
2025-04-26 21:00:51 -0700

    chore: update nuxt configuration

--- layers/emails/nuxt.config.ts
+++ layers/emails/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+	devtools: { enabled: true },
 	compatibilityDate: '2024-07-21',
 	extends: ['@repo/ui'],
 	modules: ['@vue-email/nuxt'],
